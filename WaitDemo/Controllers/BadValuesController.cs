bcb7040a3592d94b55b30851de4817165f0d56f3
aensidhe <me@aensidhe.ru>
2014-06-14 16:08:07 +0400

    Demo for ThreadPool is ready

---
+++ WaitDemo/Controllers/BadValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WaitDemo.Controllers
+{
+	public class BadValuesController : ApiController
+	{
+		// GET api/values
+		public IEnumerable<string> Get()
+		{
+			Thread.Sleep(2000);
+			return new string[] { "value1", "value2" };
+		}
+	}
+}
---
+++ WaitDemo/Controllers/GoodValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WaitDemo.Controllers
+{
+	public class GoodValuesController : ApiController
+	{
+		// GET api/values
+		public async Task<IEnumerable<string>> Get()
+		{
+			var t = Task.Delay(2000);
+			await t;
+			return new string[] { "value1", "value2" };
+		}
+	}
+}
