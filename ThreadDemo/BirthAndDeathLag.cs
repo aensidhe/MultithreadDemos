e17d2c38d5267117e5be88874e5bc6acc5d78517
aensidhe <me@aensidhe.ru>
2014-06-15 11:44:44 +0400

    Thread is created only on Start.

---
+++ ThreadDemo/BirthAndDeathLag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadDemo
+{
+    public class BirthAndDeathLag
+    {
+
+		[DllImport("NativeDll.dll")]
+		private static extern int fnNativeDll();
+
+		public int GetSomeInt()
+		{
+			return fnNativeDll();
+		}
+
+		public int GetSomeIntWithThread()
+		{
+			var t = new Thread(_ => { Console.WriteLine("ManagedThreadId = {0}", Thread.CurrentThread.ManagedThreadId); });
+			t.Start();
+			t.Join();
+			return fnNativeDll();
+		}
+
+		public int GetSomeIntWithThreadThatDidntStart()
+		{
+			var t = new Thread(_ => { Console.WriteLine("ManagedThreadId = {0}", Thread.CurrentThread.ManagedThreadId); });
+			return fnNativeDll();
+		}
+    }
+}
---
+++ ThreadDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadDemo
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			GetIntWithMeasure(x => x.GetSomeInt());
+			GetIntWithMeasure(x => x.GetSomeIntWithThread());
+			GetIntWithMeasure(x => x.GetSomeIntWithThreadThatDidntStart());
+		}
+
+		private static void GetIntWithMeasure(Func<BirthAndDeathLag, int> f)
+		{
+			var s = new Stopwatch();
+			s.Start();
+			int result;
+			try
+			{
+				var a = new BirthAndDeathLag();
+				result = f(a);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				result = 0;
+			}
+			finally
+			{
+				s.Stop();
+			}
+
+			Console.WriteLine("Some int = {0}", result);
+			Console.WriteLine("{0} ms. Press any key to continue", s.ElapsedMilliseconds);
+			Console.ReadKey();
+		}
+	}
+}
