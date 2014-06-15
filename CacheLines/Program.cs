4d92d0a24a27835e7bf678cd135250a3f02f6227
aensidhe <me@aensidhe.ru>
2014-06-15 17:47:07 +0400

    Cache lines demo. Wrong demo.

---
+++ CacheLines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wintellect.Threading.LogicalProcessor;
+
+namespace CacheLines
+{
+	internal class Program
+	{
+		internal static void Main(string[] args)
+		{
+			TestAll();
+			TestAll();
+
+			Console.ReadKey();
+		}
+
+		private static void TestAll()
+		{
+			Console.WriteLine("Struct auto");
+			new TestStruct32().Test().Wait();
+
+			Console.WriteLine("Struct sequential");
+			new TestStruct32Sequential().Test().Wait();
+
+			Console.WriteLine("Struct explicit");
+			new TestStruct32Explicit().Test().Wait();
+
+			Console.WriteLine("Struct explicit 64");
+			new TestStruct64().Test().Wait();
+
+			Console.WriteLine("Class");
+			new TestClass().Test().Wait();
+		}
+
+		
+	}
+}
